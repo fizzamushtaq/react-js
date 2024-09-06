b86003958e0bd060ea23517f941ba901a4fe5757
Fizza <fizach581@gmail.com>
2024-09-07 01:50:15 +0500

    todo-list
    todo list with the help of redux toolkit

---
+++ PRACTICE-Projects/redux-todo-project/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Addtodo from './components/Addtodo'
+import Todos from './components/todos'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1> testing</h1>
+      <Addtodo/>
+      <Todos/>
+    </>
+    
+  )
+}
+
+export default App
